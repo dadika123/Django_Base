1f7d56da4c3dbcb9edbe4506cd0ba407973899e6
Chuzhikov Danil <danil.chuzhikov@gmail.com>
2021-03-04 12:53:50 +0300

    homework_3

---
+++ mainapp/urls.py
@@ -0,0 +1,8 @@
+import mainapp.views as mainapp
+from django.urls import path
+
+app_name = 'mainapp'
+urlpatterns = [
+    path('', mainapp.products, name='index'),
+    path('<int:pk>/', mainapp.products, name=''),
+]
