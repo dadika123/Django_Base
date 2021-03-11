c485e10bb0f7e21a96c010dc7d03ca03270c8531
Chuzhikov Danil <danil.chuzhikov@gmail.com>
2021-03-11 19:51:15 +0300

    urls for basket

---
+++ basketapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from basketapp.views import basket_add
+
+app_name = 'basketapp'
+
+urlpatterns = [
+    path(),
+]
--- basketapp/views.py
+++ basketapp/views.py
@@ -1,8 +1,13 @@
 from mainapp.views import products
-from django.shortcuts import HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect, render
 from mainapp.models import Product
 from basketapp.models import Basket
 from django.urls import reverse
+
+
+def basket(request):
+    content = {}
+    return render(request, 'basketapp/basket.html', content)
 
 
 def basket_add(request, product_id=None):
@@ -19,3 +24,7 @@ def basket_add(request, product_id=None):
         basket.quantity += 1
         basket.save()
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def basket_remove(request, product_id=None):
+    content={}
+    return render(request, 'basketapp/basket.html', content)
