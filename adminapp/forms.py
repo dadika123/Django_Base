93aabe322dd56783f4d54472abac3f6d24b1d488
Chuzhikov Danil <danil.chuzhikov@gmail.com>
2021-03-24 02:43:44 +0300

    create read

---
+++ adminapp/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from authapp.forms import ShopUserRegisterForm, ShopUserProfileForm
+from authapp.models import ShopUser
+
+
+class NewAdminRegisterForm(ShopUserRegisterForm):
+    avatar = forms.ImageField(widget=forms.FileInput(), required=False)
+
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'email', 'avatar', 'first_name', 'last_name', 'age', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(NewAdminRegisterForm, self).__init__(*args, **kwargs)
+        self.fields['avatar'].widget.attrs['class'] = 'custom-file-input'
+
+
+class NewAdminProfileForm(ShopUserProfileForm):
+
+    def __init__(self, *args, **kwargs):
+        super(NewAdminProfileForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['readonly'] = False
+        self.fields['email'].widget.attrs['readonly'] = False
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from adminapp.forms import NewAdminRegisterForm
+from authapp.models import ShopUser
 
 
 # Create your views here.
@@ -7,11 +10,20 @@ def index(request):
 
 
 def admin_users(request):
-    return render(request, 'adminapp/admin-users-read.html')
+    context = {'users': ShopUser.objects.all()}
+    return render(request, 'adminapp/admin-users-read.html', context)
 
 
 def admin_users_create(request):
-    return render(request, 'adminapp/admin-users-create.html')
+    if request.method == 'POST':
+        register_form = NewAdminRegisterForm(data=request.POST, files=request.FILES)
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('new_admin:admin_users'))
+    else:
+        register_form = NewAdminRegisterForm()
+    context = {'register_form': register_form}
+    return render(request, 'adminapp/admin-users-create.html', context)
 
 
 def admin_users_update(request):
