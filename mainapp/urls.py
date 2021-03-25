02330b32cea32b5a0f9e2469791b359bb9583381
Chuzhikov Danil <danil.chuzhikov@gmail.com>
2021-03-25 05:38:49 +0300

    min fix urls

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,10 +1,10 @@
-import mainapp.views as mainapp
 from django.urls import path
+
+import mainapp.views as mainapp
 
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.products, name='index'),
-    path('<int:pk>/', mainapp.products, name='product'),
     path('category/<int:category_id>/', mainapp.products, name='category'),
 ]
