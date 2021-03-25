e433b63d3bc4230b9d013ea9c47176ad6e5d2058
chuzhikov_danil <danil.chuzhikov@gmail.com>
2021-03-26 00:01:19 +0300

    start migrate to CBV

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 
-from adminapp.views import admin_users, admin_users_create, admin_users_update, admin_users_delete, index, \
-    admin_categories, admin_category_create
+from adminapp.views import admin_users_update, admin_users_delete, index, \
+    admin_categories, admin_category_create, UserListView, UserCreateView
 
 app_name = 'adminapp'
 
 urlpatterns = [
     path('', index, name='index'),
     path('users-delete/<int:user_id>/', admin_users_delete, name='admin_users_delete'),
-    path('users/', admin_users, name='admin_users'),
+    path('users/', UserListView.as_view(), name='admin_users'),
     path('categories/', admin_categories, name='admin_categories'),
     path('categories-create/', admin_category_create, name='admin_category_create'),
     path('users-update/<int:user_id>/', admin_users_update, name='admin_users_update'),
-    path('users-create/', admin_users_create, name='admin_users_create'),
+    path('users-create/', UserCreateView.as_view(), name='admin_users_create'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import User
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
-
+from django.urls import reverse, reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from adminapp.forms import NewAdminRegisterForm, NewAdminProfileForm, NewAdminProductCategoryForm
 from authapp.models import ShopUser
 from geekshop.settings import MEDIA_URL
@@ -13,24 +15,34 @@ def index(request):
     return render(request, 'adminapp/admin.html')
 
 
-@user_passes_test(lambda u: u.is_superuser, login_url='/')
-def admin_users(request):
-    context = {'users': ShopUser.objects.all()}
-    return render(request, 'adminapp/admin-users-read.html', context)
+class UserListView(ListView):
+    model = ShopUser
+    template_name = 'adminapp/admin-users-read.html'
+
+# @user_passes_test(lambda u: u.is_superuser, login_url='/')
+# def admin_users(request):
+#     context = {'users': ShopUser.objects.all()}
+#     return render(request, 'adminapp/admin-users-read.html', context)
 
 
-@user_passes_test(lambda u: u.is_superuser, login_url='/')
-def admin_users_create(request):
-    if request.method == 'POST':
-        register_form = NewAdminRegisterForm(
-            data=request.POST, files=request.FILES)
-        if register_form.is_valid():
-            register_form.save()
-            return HttpResponseRedirect(reverse('new_admin:admin_users'))
-    else:
-        register_form = NewAdminRegisterForm()
-    context = {'register_form': register_form}
-    return render(request, 'adminapp/admin-users-create.html', context)
+class UserCreateView(CreateView):
+    model = ShopUser
+    template_name = 'adminapp/admin-users-create.html'
+    form_class = NewAdminRegisterForm
+    success_url = reverse_lazy('new_admin:admin_users')
+
+# @user_passes_test(lambda u: u.is_superuser, login_url='/')
+# def admin_users_create(request):
+#     if request.method == 'POST':
+#         register_form = NewAdminRegisterForm(
+#             data=request.POST, files=request.FILES)
+#         if register_form.is_valid():
+#             register_form.save()
+#             return HttpResponseRedirect(reverse('new_admin:admin_users'))
+#     else:
+#         register_form = NewAdminRegisterForm()
+#     context = {'register_form': register_form}
+#     return render(request, 'adminapp/admin-users-create.html', context)
 
 
 @user_passes_test(lambda u: u.is_superuser, login_url='/')
